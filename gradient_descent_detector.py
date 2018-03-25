3dc488597b82c6d4e8898ed40f8bebaf598f0572
Yang Liu <byliuyang11@gmail.com>
2018-03-25 17:08:54 -0400

    Finish gradient descent

--- gradient_descent_detector.py
+++ gradient_descent_detector.py
@@ -7,32 +7,34 @@ def accuracy(y, yhat):
 def mse(y, yhat):
     return 0.5 * np.mean((yhat - y) ** 2)
 
-def yhat(X, w):
-    return X.T.dot(w)
+def yhat(X, w, b):
+    return X.T.dot(w) + b
 
 def gradient_w(X, y, w, b):
-    return X.dot(yhat(X, w) - y)
+    return X.dot(yhat(X, w, b) - y) / X.shape[1]
 
 def gradient_b(X, y, w, b):
-    return 1
+    return np.mean(yhat(X, w, b) - y)
 
-def error(X, y, w):
-    return mse(y, yhat(X, w))
+def error(X, y, w, b):
+    return mse(y, yhat(X, w, b))
 
 def smile_classifier(trainingFaces, trainingLabels):
-    learning_rate = .000008
-    tolerance = .00000001
+    learning_rate = .005
+    tolerance = .000001
 
     X = trainingFaces.T
     y = trainingLabels
     w = np.random.rand(X.shape[0])
+    b = np.random.rand()
 
-    prev_error = error(X, y, w)
+    prev_error = error(X, y, w, b)
 
     is_training = True
 
     while is_training:
-        w = w - learning_rate * gradient(X, y, w)
+        w = w - learning_rate * gradient_w(X, y, w, b)
+        b = b - learning_rate * gradient_b(X, y, w, b)
         current_error = error(X, y, w, b)
 
         if abs(prev_error - current_error) < tolerance:
@@ -41,11 +43,11 @@ def smile_classifier(trainingFaces, trainingLabels):
             prev_error = current_error
     
     def predict(faces):
-        predictions =  yhat(faces.T, w)
+        predictions =  yhat(faces.T, w, b)
         return [0 if prediction <= 0.5 else 1 for prediction in predictions]
 
-    Classifier =  namedtuple("Classifier", ["w", "predict"])
-    return Classifier(w, predict)
+    Classifier =  namedtuple("Classifier", ["w", "b", "predict"])
+    return Classifier(w, b, predict)
 
 def detect_smile(trainingFaces, trainingLabels, testingFaces, testingLabels):
 
